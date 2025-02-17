211efc55b018220d20e27091547b56fab3372562
Finlay <finman292004@protonmail.com>
2025-02-17 14:55:51 -0500

    Update start.sh

--- engfrosh-site/start.sh
+++ engfrosh-site/start.sh
@@ -5,3 +5,4 @@ cd engfrosh/engfrosh_site/
 nginx
 rm -f /home/ubuntu/engfrosh/engfrosh_site/engfrosh_site.sock /home/ubuntu/engfrosh/engfrosh_site/engfrosh_site.sock.lock
 sudo -E -u ubuntu -g www-data uvicorn --workers $WORKERS --proxy-headers --uds /home/ubuntu/engfrosh/engfrosh_site/engfrosh_site.sock engfrosh_site.asgi:application
+chmod 666 /home/ubuntu/engfrosh/engfrosh_site/engfrosh_site.sock
